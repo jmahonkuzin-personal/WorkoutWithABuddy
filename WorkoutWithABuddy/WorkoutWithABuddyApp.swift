977f501ec036a804759e7d856ae7c28a6c62795f
Julia Mahon Kuzin <j.mahonkuzin@gmail.com>
2024-09-24 17:44:10 -0400

    Initial Commit

---
+++ WorkoutWithABuddy/WorkoutWithABuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutWithABuddyApp.swift
+//  WorkoutWithABuddy
+//
+//  Created by Julia Mahon Kuzin on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutWithABuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
